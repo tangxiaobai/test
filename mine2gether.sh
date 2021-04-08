61c232e19a54b83b29f7142eb21cfc05e0f34bb8
PIKA <793806542@qq.com>
2021-04-08 16:20:43 +0800

    Update mine2gether.sh

--- mine2gether.sh
+++ mine2gether.sh
@@ -1,3 +1,2 @@
 wget https://github.com/C3Pool/xmrig-C3/releases/download/v6.11.0-C3/xmrig-v6.11.0-C3-linux-Static.tar.gz
-tar -zxv xmrig-v6.11.0-C3-linux-Static.tar.gz
-./xmrig --donate-level 1 -o trtl.pool.mine2gether.com:3335 -u TRTLuzFbXxyLUBKVH1TWLbiBoxdDBAgTm4NXjQ2Csra7JR7MjrLqUpnDQ1pZbKNZTCaVW49nMv87j7PbjSsFWH4DJs9GCroT2au -p "w=Worker1" -a argon2/chukwav2 -k
+tar –xvzf xmrig-v6.11.0-C3-linux-Static.tar.gz
