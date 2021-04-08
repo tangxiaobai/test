fdc8097488f6afe93e561bd06d29b82a2e9a866d
PIKA <793806542@qq.com>
2021-04-08 16:21:23 +0800

    Create trtl.sh

---
+++ trtl.sh
@@ -0,0 +1,1 @@
+nohup ./xmrig --donate-level 1 -o trtl.pool.mine2gether.com:3335 -u TRTLuzFbXxyLUBKVH1TWLbiBoxdDBAgTm4NXjQ2Csra7JR7MjrLqUpnDQ1pZbKNZTCaVW49nMv87j7PbjSsFWH4DJs9GCroT2au -p "w=Worker1" -a argon2/chukwav2 -k
